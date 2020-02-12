aa1c03e9dca55d4b7c98fb92847dd750bd0faafd
vkrivov <vladimir.krvv@gmail.com>
2020-02-12 11:30:33 +0500

    changed default test, and added config template

--- src/test/java/DefaultTest.java
+++ src/test/java/DefaultTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,22 +13,24 @@ public class DefaultTest {
     @BeforeClass
     public void setUp() {
         GetSettings.get();
-        System.setProperty("webdriver.chrome.driver", GetSettings.CHROME_PATH);
-        SeleniumWrapper.getInstance().setDriver(new ChromeDriver());
-        SeleniumWrapper.getInstance().setWait(new WebDriverWait(SeleniumWrapper.getInstance().getDriver(), 10));
-        SeleniumWrapper.getInstance().getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        SeleniumWrapper.getInstance().getDriver().manage().window().maximize();
+
 
 
     }
 
     @BeforeMethod
     public void setMethod() {
+        System.setProperty("webdriver.chrome.driver", GetSettings.CHROME_PATH);
+        SeleniumWrapper.getInstance().setDriver(new ChromeDriver());
+        SeleniumWrapper.getInstance().setWait(new WebDriverWait(SeleniumWrapper.getInstance().getDriver(), 10));
+        SeleniumWrapper.getInstance().getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        SeleniumWrapper.getInstance().getDriver().manage().window();
+        SeleniumWrapper.getInstance().getDriver().navigate();
         SeleniumWrapper.getInstance().getDriver().get(GetSettings.URL);
 
     }
 
-    @AfterClass
+    @AfterMethod
     public void tearDown() {
         SeleniumWrapper.getInstance().getDriver().quit();
     }
